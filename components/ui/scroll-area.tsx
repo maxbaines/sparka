a1b90b61393ca9e173d91d52f8aad5b26b04b726
Francisco Moretti <franciscoemoretti@gmail.com>
2025-09-27 10:43:09 +0100

    Add block display for child elements in ScrollArea component
    - Updated the ScrollArea component to include a new class '[&>div]:!block', ensuring that child elements are displayed as block elements. This change enhances layout control and improves the overall rendering of the scroll area.
    
    These adjustments contribute to a more consistent and user-friendly UI experience.

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -22,6 +22,7 @@ const ScrollArea = React.forwardRef<
         ref={ref} // Changed to the viewport for stick to bottom interaction
         className={cn(
           'focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1',
+          '[&>div]:!block',
           viewPortClassName,
         )}
       >
